ab6de75a85ef8dc43402178f0b014cc844ebafc9
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-03-15 18:34:13 -0400

    random color complete

--- 3.3_random_color/src/App.js
+++ 3.3_random_color/src/App.js
@@ -1,12 +1,19 @@
 import React, {useState, useEffect} from 'react'
+const axios = require('axios');
 
 export default function App() {
+    const [color, setColor] = useState(`blue`)
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
+            .then(res => setColor(`#${res.data.new_color}`))
+            .catch(err => console.error(err))
+    }, [count])
     return (
-        
-
-
-        <div>
-
+        <div style={{backgroundColor: color}} className="box">
+            <h1>Box Color</h1>
+            <button onClick={() => setCount(prev => prev +1)}>Change Color</button>
         </div>
     )
 }
